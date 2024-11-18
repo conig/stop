525f807d3c32b2cb5d604c95b00ccaeed2ba1dde
James Conigrave <james.conigrave@gmail.com>
2024-11-19 10:29:39 +1100

    add safe_numeric

---
+++ R/safe_numeric.R
@@ -0,0 +1,20 @@
+#' safe_numeric
+#'
+#' Convert a vector to numeric, but stop if unknown values could not be converted
+#'
+#' @param x A vector to convert to numeric
+#' @param ignore A vector of values to ignore when checking for unknown fitted.values#' @return A numeric vector
+#' @export
+
+safe_numeric <- function(x, ignore = c()) {
+  # compare the original vector with the numeric conversion
+  suppressWarnings(x_num <- as.numeric(x))
+  vals <- x[is.na(x_num)]
+  u_vals <- na.omit(unique(vals))
+  u_vals <- u_vals[!u_vals %in% ignore]
+  # Warn if unknown charcters being eliminated
+  if (length(u_vals) > 0) {
+    stop(paste("The following values could not be converted to numeric: ", paste(u_vals, collapse = ", ")))
+  }
+  x_num
+}
