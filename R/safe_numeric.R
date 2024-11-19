7eb6270213ebc746b16a285eba55c28ed13cc06f
James Conigrave <james.conigrave@gmail.com>
2024-11-19 13:37:47 +1100

    use a more logical argument to ignore all

--- R/safe_numeric.R
+++ R/safe_numeric.R
@@ -4,17 +4,17 @@
 #'
 #' @param x A vector to convert to numeric
 #' @param ignore A vector of values to ignore when checking for unknown values
-#' @param use_ignore A logical value indicating whether to use the ignore vector
+#' @param ignore_all A logical value indicating whether all chars should be ignored
 #' @return A numeric vector
 #' @export
 
-safe_numeric <- function(x, ignore = c(), use_ignore = TRUE) {
+safe_numeric <- function(x, ignore = c(), ignore_all = FALSE) {
   # compare the original vector with the numeric conversion
   suppressWarnings(x_num <- as.numeric(x))
   vals <- x[is.na(x_num)]
   u_vals <- na.omit(unique(vals))
   # If ignore is set to NULL, do not perform checks
-  if (use_ignore) {
+  if (!ignore_all) {
     u_vals <- u_vals[!u_vals %in% ignore]
     # Warn if unknown charcters being eliminated
     if (length(u_vals) > 0) {
