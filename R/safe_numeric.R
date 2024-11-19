46b32297002704b712ba13a2772b6ead37582eac
James Conigrave <james.conigrave@gmail.com>
2024-11-19 13:34:25 +1100

    make the setting to turn off safe ignore explicit

--- R/safe_numeric.R
+++ R/safe_numeric.R
@@ -4,16 +4,17 @@
 #'
 #' @param x A vector to convert to numeric
 #' @param ignore A vector of values to ignore when checking for unknown values
+#' @param use_ignore A logical value indicating whether to use the ignore vector
 #' @return A numeric vector
 #' @export
 
-safe_numeric <- function(x, ignore = c()) {
+safe_numeric <- function(x, ignore = c(), use_ignore = TRUE) {
   # compare the original vector with the numeric conversion
   suppressWarnings(x_num <- as.numeric(x))
   vals <- x[is.na(x_num)]
   u_vals <- na.omit(unique(vals))
   # If ignore is set to NULL, do not perform checks
-  if (!is.null(ignore)) {
+  if (use_ignore) {
     u_vals <- u_vals[!u_vals %in% ignore]
     # Warn if unknown charcters being eliminated
     if (length(u_vals) > 0) {
